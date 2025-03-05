4026a76ecac4a648a6b1784d165cd8c36867f3bb
David Santiago <ds.tapias@uniandes.edu.co>
2025-03-04 21:33:24 -0500

    Conexión del back con el front

--- Last-Bite-Backend/app.py
+++ Last-Bite-Backend/app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Allows requests from React
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    return jsonify({"message": "Hello from Flask!"})
 
 @app.route('/')
 def home():
