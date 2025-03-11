4dbc94a49088f48db58d8de08c3ae97324394257
David Santiago <ds.tapias@uniandes.edu.co>
2025-03-10 23:40:45 -0500

    Creation of the folder with constants

---
+++ Last-Bite-Backend/enumerations/enums.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+class UserType(Enum):
+    CUSTOMER = 'CUSTOMER'
+    DELIVERY = 'DELIVERY'
+    STORE = 'STORE'
+
+class ProductType(Enum):
+    PRODUCT = 'PRODUCT'
+    SUBSCRIPTION = 'SUBSCRIPTION'
+
+class SubscriptionType(Enum):
+    WEEK = 'WEEK'
+    MONTH = 'MONTH'
+    YEAR = 'YEAR'
+
+class Status(Enum):
+    ACTIVE = 'ACTIVE'
+    PAYMENT_PROGRESS = 'PAYMENT_PROGRESS'
+    BILLED = 'BILLED'
+    DISABLED = 'DISABLED'
