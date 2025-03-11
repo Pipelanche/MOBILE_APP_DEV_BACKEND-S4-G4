9a18081ea4ee5262aeb32aa1f0b446d24e72ffe9
David Santiago <ds.tapias@uniandes.edu.co>
2025-03-10 23:57:38 -0500

    hot fix for import in flask

---
+++ Last-Bite-Backend/enumerations/__init__.py
@@ -0,0 +1,8 @@
+# app/enumerations/__init__.py
+
+from .user_type import UserType
+from .product_type import ProductType
+from .subscription_type import SubscriptionType
+from .status import Status
+
+__all__ = ["UserType", "ProductType", "SubscriptionType", "Status"]
