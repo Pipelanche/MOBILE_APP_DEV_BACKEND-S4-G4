7ee10ea5cab04f5126109d463f5c7b6213c037fb
David Santiago <ds.tapias@uniandes.edu.co>
2025-03-20 00:14:12 -0500

    update postman and insertions for stores in bogota and girardot

--- Last-Bite-Backend/docs/insert_stores.sql
+++ Last-Bite-Backend/docs/insert_stores.sql
@@ -9,3 +9,27 @@ INSERT INTO store (nit, name, address, longitude, latitude, logo) VALUES
 ('903876543-8', 'Fitness Gear', '852 Gym St, Pereira', -75.6961, 4.8143, 'https://example.com/logo8.png'),
 ('904567890-9', 'Coffee Lovers', '963 Java Ave, Armenia', -75.6795, 4.5339, 'https://example.com/logo9.png'),
 ('904987654-10', 'ElectroWorld', '258 Electric St, Cúcuta', -72.5078, 7.8939, 'https://example.com/logo10.png');
+
+--BOGOTA
+insert into STORE (address, latitude, logo, longitude, name, nit, store_id ) values ('8th Floor', 4.7541706271, 'https://picsum.photos/350', -74.08625703, 'Nlounge', 340167108, 11);
+insert into STORE (address, latitude, logo, longitude, name, nit, store_id ) values ('Suite 98', 4.7808241926, 'https://picsum.photos/350', -74.0438694246, 'Jaxnation', 712253978, 12);
+insert into STORE (address, latitude, logo, longitude, name, nit, store_id ) values ('12th Floor', 4.7717550465, 'https://picsum.photos/350', -74.1111937384, 'Topicstorm', 560394616, 13);
+insert into STORE (address, latitude, logo, longitude, name, nit, store_id ) values ('Suite 34', 4.7470401245, 'https://picsum.photos/350', -74.0438694246, 'Photofeed', 586554260, 14);
+insert into STORE (address, latitude, logo, longitude, name, nit, store_id ) values ('Apt 1129', 4.8058962677, 'https://picsum.photos/350', -74.058454808, 'Teklist', 172978945, 15);
+insert into STORE (address, latitude, logo, longitude, name, nit, store_id ) values ('PO Box 22324', 4.7654601425, 'https://picsum.photos/350', -74.0707640695, 'Buzzbean', 661672420, 16);
+insert into STORE (address, latitude, logo, longitude, name, nit, store_id ) values ('Apt 1501', 4.8174776639, 'https://picsum.photos/350', -74.0407268455, 'Talane', 732621049, 17);
+insert into STORE (address, latitude, logo, longitude, name, nit, store_id ) values ('PO Box 91366', 4.7830556603, 'https://picsum.photos/350', -74.0981548962, 'Topicblab', 884841226, 18);
+insert into STORE (address, latitude, logo, longitude, name, nit, store_id ) values ('8th Floor', 4.8364426635, 'https://picsum.photos/350', -74.0306918478, 'Youtags', 661879279, 19);
+insert into STORE (address, latitude, logo, longitude, name, nit, store_id ) values ('Suite 98', 4.7712460899, 'https://picsum.photos/350', -74.0383699113, 'Zooveo', 739218345, 20);
+
+--Girardot
+insert into STORE (address, latitude, logo, longitude, name, nit, store_id ) values ('PO Box 85294', 4.2969263821, 'https://picsum.photos/350', -74.7718930335, 'Abata', 520471467, 21);
+insert into STORE (address, latitude, logo, longitude, name, nit, store_id ) values ('Room 148', 4.3028783578, 'https://picsum.photos/350', -74.7653507909, 'Meeveo', 364727056, 22);
+insert into STORE (address, latitude, logo, longitude, name, nit, store_id ) values ('15th Floor', 4.2829441305, 'https://picsum.photos/350', -74.7718930335, 'Livetube', 625556943, 23);
+insert into STORE (address, latitude, logo, longitude, name, nit, store_id ) values ('Suite 57', 4.2885973859, 'https://picsum.photos/350', -74.7737801137, 'Livefish', 799782206, 24);
+insert into STORE (address, latitude, logo, longitude, name, nit, store_id ) values ('PO Box 3128', 4.2885973859, 'https://picsum.photos/350', -74.7896346988, 'Dabjam', 379779448, 25);
+insert into STORE (address, latitude, logo, longitude, name, nit, store_id ) values ('Apt 715', 4.2896921518, 'https://picsum.photos/350', -74.7718930335, 'Devify', 700376435, 26);
+insert into STORE (address, latitude, logo, longitude, name, nit, store_id ) values ('Suite 14', 4.2820275539, 'https://picsum.photos/350', -74.7932030414, 'InnoZ', 861930917, 27);
+insert into STORE (address, latitude, logo, longitude, name, nit, store_id ) values ('PO Box 52306', 4.2885973859, 'https://picsum.photos/350', -74.7551772758, 'Realblab', 825152494, 28);
+insert into STORE (address, latitude, logo, longitude, name, nit, store_id ) values ('Suite 17', 4.2885973859, 'https://picsum.photos/350', -74.755523475, 'Youbridge', 976902595, 29);
+insert into STORE (address, latitude, logo, longitude, name, nit, store_id ) values ('Suite 3', 4.3146284448, 'https://picsum.photos/350', -74.7761234576, 'Kwilith', 734885095, 30);
