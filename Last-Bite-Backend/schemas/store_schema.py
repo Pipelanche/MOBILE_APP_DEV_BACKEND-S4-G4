377c38c88c6ad295842f02d1c0abdb89e0b277fa
Andrés Romero <af.romerob1@uniandes.edu.co>
2025-05-15 14:03:13 -0500

    Feat/update delete store (#54)
    * Fix migrations
    
    * Update Stores table
    
    * Add update and delete store features

--- Last-Bite-Backend/schemas/store_schema.py
+++ Last-Bite-Backend/schemas/store_schema.py
@@ -1,10 +1,14 @@
 from marshmallow import Schema, fields, validate
 
 class StoreSchema(Schema):
-    store_id = fields.Int(dump_only=True)  # Auto-generated ID
+    store_id = fields.Int(required=False)  # Auto-generated ID
     nit = fields.Str(required=True, validate=validate.Length(min = 10,max=150))
     name = fields.Str(required=True, validate=validate.Length(min = 1,max=150))
     address = fields.Str(required=True, validate=validate.Length(min = 1,max=240))
     longitude = fields.Float(required=True)
     latitude = fields.Float(required=True)
-    logo = fields.Str(required=True, validate=validate.Length(min = 1,max=1000))
+    logo = fields.Str(required=False, validate=validate.Length(min = 1,max=1000))
+    opens_at = fields.Time(required=True)
+    closes_at = fields.Time(required=True)
+    created_at = fields.DateTime(dump_only=True)  # Auto-generated timestamp
+    updated_at = fields.DateTime(dump_only=True)  # Auto-generated timestamp
