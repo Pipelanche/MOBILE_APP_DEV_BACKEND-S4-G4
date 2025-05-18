68a52b1f074c663328a16fe0cfd595a1884f39bd
Pipelanche <j.lancherosc@uniandes.edu.co>
2025-05-18 15:38:08 -0500

    Migrations fixed

--- Last-Bite-Backend/migrations/versions/35108122efee_quitar_id_de_events.py
+++ Last-Bite-Backend/migrations/versions/35108122efee_quitar_id_de_events.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '35108122efee'
-down_revision = 'afe74cf6bf8d'
+down_revision = '4e3a4579fb27'
 branch_labels = None
 depends_on = None
 
