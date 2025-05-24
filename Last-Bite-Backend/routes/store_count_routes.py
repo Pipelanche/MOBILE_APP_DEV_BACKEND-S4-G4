e04120edc179d3e52e6378d2f3df4eb5fd29d837
Pipelanche <j.lancherosc@uniandes.edu.co>
2025-05-24 03:24:21 -0500

    Favourite Store BQ

--- Last-Bite-Backend/routes/store_count_routes.py
+++ Last-Bite-Backend/routes/store_count_routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify, request
-from services.store_count_service import generate_store_count, get_store_count_by_store_and_user_id, update_store_count
+from services.store_count_service import generate_store_count, get_store_count_by_store_and_user_id, update_store_count, get_top_store_by_user_id
+from services.store_service import get_store_by_id
 from schemas.store_count_schema import StoreCountSchema
+from schemas.store_schema import StoreSchema
 
 
 store_count_bp = Blueprint("store_count_bp", __name__)
@@ -8,6 +10,7 @@ store_count_bp = Blueprint("store_count_bp", __name__)
 # Initialize schemas
 store_count_schema = StoreCountSchema()
 store_counts_schema = StoreCountSchema(many=True)
+store_schema = StoreSchema()
 
 # GENERATE a new store count (with schema validation)
 @store_count_bp.route("/", strict_slashes=False, methods=["POST"])
@@ -34,3 +37,11 @@ def get_store_counts_all():
 
     store_counts_list = get_store_counts_all()
     return jsonify(store_counts_schema.dump(store_counts_list)), 200
+
+# GET all Top 3 Stores 
+@store_count_bp.route("/top1/<int:user_id>", strict_slashes=False, methods=["GET"])
+def get_top_3_stores(user_id):
+
+    top_store_id_by_user_id = get_top_store_by_user_id(user_id)
+    get_full_top_store_info = get_store_by_id(top_store_id_by_user_id.store_id)
+    return jsonify(store_schema.dump(get_full_top_store_info)), 200
